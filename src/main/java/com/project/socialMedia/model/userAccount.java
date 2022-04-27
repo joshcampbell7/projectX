d8e725f7e1d41cd43897bd3aaaec20df41204408
joshcampbell7 <joshuacampbell49@yahoo.co.uk>
2022-04-27 19:42:43 +0100

    Added userAccount

---
+++ src/main/java/com/project/socialMedia/model/userAccount.java
@@ -0,0 +1,72 @@
+package com.project.socialMedia.model;
+
+
+public class userAccount {
+    private String userID;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private String yearsOfExperience;
+
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(String yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    @Override
+    public String toString() {
+        return "userAccount{" +
+                "userID='" + userID + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", yearsOfExperience='" + yearsOfExperience + '\'' +
+                '}';
+    }
+}
