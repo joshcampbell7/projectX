afcef633c81b3ca89562bd975064cd343d2e3fe4
joshcampbell7 <joshuacampbell49@yahoo.co.uk>
2022-04-27 19:30:10 +0100

    Init of the Spring Boot project + started working on home and signup page

---
+++ src/main/java/com/project/socialMedia/controllers/mainController.java
@@ -0,0 +1,28 @@
+package com.project.socialMedia.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+public class mainController {
+
+
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+
+
+
+
+}
+
+
